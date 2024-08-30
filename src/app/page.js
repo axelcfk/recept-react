6eaa4a2496edbb65f6a85ad49fd884038f05a138
axelcfk <131769961+axelcfk@users.noreply.github.com>
2024-08-30 10:53:43 +0200

    .

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
 
   const placeholderImage = "spaghetti-bolognese.jpg";
   const [searchedIngredients, setSearchedIngredients] = useState(null);
-  let searchedInstructions = null;
-  let searchedImage = null;
+  const [searchedInstructions, setSearchedInstructions] = useState(null);
+  const [searchedImage, setSearchedImage] = useState(null);
 
   function uploadImage(e) {
     const file = e.target.files[0];
@@ -21,10 +21,49 @@ export default function Home() {
 
       reader.readAsDataURL(file);
     }
-    return (
-      <main>
-        <h1>hahah</h1>
-      </main>
-    );
   }
+  return (
+    <main>
+      <div class="container">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="search any recipe..."
+        />
+        <button id="searchButton">Search</button>
+        <h1>Recipes and stuff</h1>
+        <div>
+          <form id="form">
+            <div>
+              <h2 id>Name</h2>
+              <input type="text" id="name" />
+            </div>
+
+            <h2>Ingredients</h2>
+            <textarea name="" id="ingredients" cols="30" rows="10"></textarea>
+
+            <h2>Instruction</h2>
+            <textarea name="" id="instruction" cols="30" rows="10"></textarea>
+
+            <h2>Image</h2>
+            <input type="file" id="image" accept="image/*" />
+
+            <button id="save">Save Recipe</button>
+          </form>
+        </div>
+
+        <div class="login-form">
+          <input type="text" placeholder="Username" id="username" />
+          <input type="password" placeholder="password" id="password" />
+          <button type="submit" id="loginButton">
+            Login
+          </button>
+          <div id="loginError"></div>
+        </div>
+
+        <div id="recipeCardsContainer" class="recipeCardsContainer"></div>
+      </div>
+    </main>
+  );
 }
