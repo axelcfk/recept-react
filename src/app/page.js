4277d60cf8d24f5c0e1d57104b12dc547887db27
axelcfk <131769961+axelcfk@users.noreply.github.com>
2024-08-30 11:18:51 +0200

    .

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,14 @@ export default function Home() {
   const [searchedIngredients, setSearchedIngredients] = useState(null);
   const [searchedInstructions, setSearchedInstructions] = useState(null);
   const [searchedImage, setSearchedImage] = useState(null);
+  const [name, setName] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [instruction, setInstruction] = useState("");
+  const [image, setImage] = useState(placeholderImage);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [valid, setValid] = useState(true);
 
+  //ladda upp bild
   function uploadImage(e) {
     const file = e.target.files[0];
 
@@ -22,9 +29,25 @@ export default function Home() {
       reader.readAsDataURL(file);
     }
   }
+
+  //submitta receptet och skapa kort
+  function SubmitRecipe(e) {
+    e.preventDefault();
+
+    if (name === "" || ingredients === "" || instruction === "") {
+      setValid(false);
+      setErrorMessage("All fields are required");
+    }
+
+    console.log("data är", name, ingredients, instruction);
+    // if (valid) {
+    //   createRecipeCard(name, ingredients, instruction, image);
+
+    // }
+  }
   return (
     <main>
-      <div class="container">
+      <div>
         <input
           type="text"
           name="search"
@@ -36,24 +59,44 @@ export default function Home() {
         <div>
           <form id="form">
             <div>
-              <h2 id>Name</h2>
-              <input type="text" id="name" />
+              <h2>Name</h2>
+              <input
+                type="text"
+                id="name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
             </div>
 
             <h2>Ingredients</h2>
-            <textarea name="" id="ingredients" cols="30" rows="10"></textarea>
+            <textarea
+              id="ingredients"
+              cols="30"
+              rows="10"
+              onChange={(e) => setIngredients(e.target.value)}
+              value={ingredients}
+            ></textarea>
 
             <h2>Instruction</h2>
-            <textarea name="" id="instruction" cols="30" rows="10"></textarea>
+            <textarea
+              id="instruction"
+              cols="30"
+              rows="10"
+              onChange={(e) => setInstruction(e.target.value)}
+              value={instruction}
+            ></textarea>
 
             <h2>Image</h2>
             <input type="file" id="image" accept="image/*" />
 
-            <button id="save">Save Recipe</button>
+            <button onClick={(uploadImage, SubmitRecipe)} id="save">
+              Save Recipe
+            </button>
+            {errorMessage !== "" && <p>{errorMessage}</p>}
           </form>
         </div>
 
-        <div class="login-form">
+        <div>
           <input type="text" placeholder="Username" id="username" />
           <input type="password" placeholder="password" id="password" />
           <button type="submit" id="loginButton">
@@ -62,7 +105,7 @@ export default function Home() {
           <div id="loginError"></div>
         </div>
 
-        <div id="recipeCardsContainer" class="recipeCardsContainer"></div>
+        <div id="recipeCardsContainer"></div>
       </div>
     </main>
   );
